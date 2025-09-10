730f4cdc640d22d00ed005dff67ed06b12262ba2
Swann LEGROS <swann.legros@dametis.com>
2025-09-10 14:20:24 +0200

    fix: better typing for books

--- types/book.ts
+++ types/book.ts
@@ -16,9 +16,9 @@ export interface BookAPISearchResult {
 export interface BookAPISearchResultItem {
   author_key: string[];
   author_name: string[];
-  cover_i: number;
+  cover_i?: number;
   edition_count: number;
-  first_publish_year: number;
+  first_publish_year?: number;
   key: string;
   language: string[];
   title: string;
